f874b99e1ae70a1419b19bbd8633bc528df15938
c0nnex <me@c0nnex.de>
2020-05-06 19:26:56 +0200

    w

--- api/AltV.Net.Async/Elements/Entities/VehicleBuilder.cs
+++ api/AltV.Net.Async/Elements/Entities/VehicleBuilder.cs
@@ -216,17 +216,7 @@ namespace AltV.Net.Async.Elements.Entities
             return this;
         }
 
-        public IVehicleBuilder Appearance(string value)
-        {
-            Add(ptr => AltNative.Vehicle.Vehicle_LoadAppearanceDataFromBase64(ptr, value));
-            return this;
-        }
-
-        public IVehicleBuilder ScriptData(string value)
-        {
-            Add(ptr => AltNative.Vehicle.Vehicle_LoadScriptDataFromBase64(ptr, value));
-            return this;
-        }
+      
 
         public async Task<IVehicle> Build()
         {
